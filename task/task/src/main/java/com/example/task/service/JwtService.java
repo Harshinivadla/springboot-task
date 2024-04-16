ef20bc2f18cb3ad135231f19ac09faa80ce0595e
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2024-04-16 14:29:52 +0530

    Add files via upload

---
+++ task/task/src/main/java/com/example/task/service/JwtService.java
@@ -0,0 +1,69 @@
+package com.example.task.service;
+
+import io.jsonwebtoken.*;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+    private static final String SECRET_KEY = "aGFyc2hpbmk=";
+
+    public String extractusername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token){
+        try{
+            return Jwts.parser()
+                    .setSigningKey(getSignKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException | MalformedJwtException e) {
+            return null;
+        }
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = extractExpiration(token);
+        return expiration != null && expiration.before(new Date());
+    }
+
+    public Boolean validateToken(String token, String username) {
+        return username.equals(extractusername(token)) && !isTokenExpired(token);
+    }
+
+    public String generateToken(String username) {
+        return createToken(username);
+    }
+
+    private String createToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 1000 * 60 * 30);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, getSignKey())
+                .compact();
+    }
+
+    private Key getSignKey() {
+        return new SecretKeySpec(SECRET_KEY.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
+    }
+}
