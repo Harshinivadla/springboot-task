d3c457de9bb1c6e3c7af8b6e218909564e88faa8
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2024-04-25 16:43:05 +0530

    Add files via upload

---
+++ task/task/src/main/java/com/example/task/entity/Contact.java
@@ -0,0 +1,24 @@
+package com.example.task.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contact_details")
+
+public class Contact {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private String mobNo;
+
+}
