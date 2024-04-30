84bfb86c46f1214e6ddb5f6a8dc65f841daca6d4
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2024-04-30 11:13:45 +0530

    Delete task/task directory

--- task/task/src/main/java/com/example/task/repository/BioIdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.task.repository;
-
-import com.example.task.entity.BioId;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BioIdRepository extends JpaRepository<BioId, String> {
-}
